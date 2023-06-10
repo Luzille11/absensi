18c3985cb486b6a38807bb9317cb5960aa088b00
unknown <ariefnurcahyo2021@gmail.com>
2023-06-10 23:05:24 +0700

    daftarpengunjung

---
+++ application/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.1
+    </div>
+    <strong> &copy; 2023 <a href="#">PUSTAKA</a>.</strong><a href='https://sharecodingan.blogspot.com/' title='sharecodingan.blogspot.com' target='_blank'></a>
+	
+</footer>
